c101563631c500c605e4667b82b66d58f43be097
Zacharia Schmitz <schmitz.zacharia@gmail.com>
2023-07-07 14:12:17 -0500

    Edit file.

--- Basic Statements.sql
+++ Basic Statements.sql
@@ -1,9 +1,12 @@
+-- SQL NOTES: 
+
+
 -- Two dashes make a single line comment in SQL
 /*
 This is
 a multi line comment */
 -- Ask the server: What section am I in presently?
-SELECT databse ();
+SELECT database ();
 -- answer: NULL (you aren't anywhere yet)
 -- SELECT => fetch me
 -- database() ==> parens means this is a function call
@@ -52,4 +55,52 @@ USE employees;
  SELECT * FROM fruits WHERE quantity BETWEEN 2 AND 5; -- Self explanatory
  
  SELECT * FROM fruits WHERE quantity >= 2 AND quantity <=4; -- Same thing, little more complex
+ 
+ Numerical Functions:
+
+1. AVG: Calculates the mean of a numerical sequence.
+   Example: SELECT AVG(sales) FROM orders;
+
+2. MIN: Finds the minimum value in a numerical sequence.
+   Example: SELECT MIN(price) FROM products;
+
+3. MAX: Finds the maximum value in a numerical sequence.
+   Example: SELECT MAX(quantity) FROM inventory;
+
+String Functions:
+
+1. CONCAT: Concatenates multiple strings or column values together.
+   Example: SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM customers;
+
+2. SUBSTR: Extracts a portion of a string based on the specified start index and length.
+   Example: SELECT SUBSTR(description, 1, 10) FROM products;
+
+3. UPPER: Converts a string to uppercase.
+   Example: SELECT UPPER(name) FROM employees;
+
+4. LOWER: Converts a string to lowercase.
+   Example: SELECT LOWER(email) FROM users;
+
+5. REPLACE: Replaces occurrences of a substring within a string with a specified replacement.
+   Example: SELECT REPLACE(text, 'old', 'new') FROM messages;
+
+Date and Time Functions:
+
+1. NOW: Retrieves the current date and time.
+   Example: SELECT NOW();
+
+2. CURDATE: Retrieves the current date.
+   Example: SELECT CURDATE();
+
+3. CURTIME: Retrieves the current time.
+   Example: SELECT CURTIME();
+
+4. UNIX_TIMESTAMP: Converts a date or date-time value to a UNIX timestamp.
+   Example: SELECT UNIX_TIMESTAMP('2023-07-06 10:30:00');
+
+Casting:
+
+Casting allows converting one data type to another.
+
+Example: SELECT CAST(quantity AS VARCHAR) FROM inventory;
  
